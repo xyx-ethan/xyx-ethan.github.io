637fb91948ec118b96c0c2f0c6b31085dc6c98a8
xyx-ethan <xyx.ethan@gmail.com>
2026-09-03 13:35:41 -0400

    Refine quartic-field partial descent without incompatible known-point hints

--- research/diophantine7/round5/descent_recon.m
+++ research/diophantine7/round5/descent_recon.m
@@ -1,4 +1,4 @@
-// Rational Diophantine 7-tuple project, round 5: descent reconnaissance.
+// Rational Diophantine 7-tuple project, round 5: refined descent reconnaissance.
 SetSeed(20260903);
 Q := Rationals();
 R<u> := PolynomialRing(Q);
@@ -6,32 +6,20 @@ h := u^2 + 4*u - 4;
 q := 49*u^4 - 100*u^3 - 12*u^2 + 128*u - 64;
 f := h*q;
 C := HyperellipticCurve(f);
-J := Jacobian(C);
 pts := RationalPoints(f,2 : Bound := 10000);
 
 print "START_DESCENT_RECON";
 rlo,rhi,gens := RankBounds(f,2 : ReturnGenerators);
 print "CYCLIC_RANK_BOUNDS", rlo,rhi;
 print "CYCLIC_GENERATORS", gens;
-for g in gens do
-    Kg<ag> := NumberField(g);
-    ok,root := IsSquare(Evaluate(f,ag));
-    print "GENERATOR_LIFTS", g,ok,root;
+for P in gens do
+    print "CYCLIC_GENERATOR_POINT", P;
 end for;
-
-K2<s2> := QuadraticField(2);
-K1543<s1543> := QuadraticField(1543);
-Km1543<sm1543> := QuadraticField(-1543);
-print "Q_OVER_QSQRT2", Factorization(PolynomialRing(K2)!q);
-print "Q_OVER_QSQRT1543", Factorization(PolynomialRing(K1543)!q);
-print "Q_OVER_QSQRTM1543", Factorization(PolynomialRing(Km1543)!q);
 
 SetVerbose("CycCov",1);
 Sel0,alg0 := qCoverPartialDescent(f,[*h,q*],2 : KnownPoints := pts, PrimeBound := 1200);
 print "PARTIAL_Q_SIZE", #Sel0;
 print "PARTIAL_Q_SET", Sel0;
-print "PARTIAL_Q_DOMAIN", Domain(alg0);
-print "PARTIAL_Q_CODOMAIN", Codomain(alg0);
 
 L<a> := NumberField(q);
 LX<X> := PolynomialRing(L);
@@ -41,7 +29,10 @@ print "L_DEGREE_SIGNATURE_CLASSNO", Degree(L), Signature(L), ClassNumber(L);
 print "L_DISC", Discriminant(Integers(L));
 print "CUBIC_FACTOR", hL*lin;
 
-Sel1,alg1 := qCoverPartialDescent(f,[*h,lin*],2 : KnownPoints := pts, PrimeBound := 1200);
+// Do not pass KnownPoints here: the refined descent has a stricter ambient
+// p-Selmer representation, and passing the full unrefined point set triggers
+// an incompatible representative error in V2.29-9.
+Sel1,alg1 := qCoverPartialDescent(f,[*h,lin*],2 : PrimeBound := 1200);
 print "PARTIAL_L_SIZE", #Sel1;
 print "PARTIAL_L_SET", Sel1;
 print "PARTIAL_L_DOMAIN", Domain(alg1);
