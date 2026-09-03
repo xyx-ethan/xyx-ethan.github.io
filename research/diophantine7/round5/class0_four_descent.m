b755f42b8a143a84d9c80e5d536c46da8ba31f61
xyx-ethan <xyx.ethan@gmail.com>
2026-09-03 14:34:33 -0400

    Test divisibility of class-0 residual Selmer element by four-descent

---
+++ research/diophantine7/round5/class0_four_descent.m
@@ -0,0 +1,29 @@
+// Determine whether the sole residual 2-Selmer class for cover class 0
+// lifts to an everywhere locally soluble 4-cover.
+SetSeed(20260903);
+Q := Rationals();
+Rb<b0> := PolynomialRing(Q);
+Fb := b0^6 + 3*b0^5 - 11*b0^4 - 27*b0^3 + 41*b0^2 + 55*b0 + 12;
+K<b> := NumberField(Fb);
+KT<T> := PolynomialRing(K);
+P := 49*T^8 - 100*T^7 + 84*T^6 - 196*T^5 + 278*T^4 - 92*T^3 - 12*T^2 + 4*T + 1;
+fac := Factorization(P);
+g := fac[1][1];
+H := HyperellipticCurve(g);
+Pbase := H![K!-1,K!2,K!1];
+E,HtoE := EllipticCurve(H,Pbase);
+ok,G,GtoE := PseudoMordellWeilGroup(E);
+assert Invariants(G) eq [2,0];
+Pfree := GtoE(G.2);
+y12 := (K!1/1092)*(-24*b^5-34*b^4+424*b^3+540*b^2-1748*b-1161);
+Pextra := HtoE(H![K!1/2,y12,K!1]);
+free := [Pfree,Pextra];
+assert IsLinearlyIndependent(free);
+covers,maps,abmap := TwoDescent(E : RemoveTorsion:=true,
+    RemoveGens:=Seqset(free), MinRed:=false, WithMaps:=true);
+assert #covers eq 1;
+print "CLASS0_RESIDUAL_COUNT", #covers;
+SetVerbose("FourDescent",2);
+four := FourDescent(covers[1] : MinRed:=false);
+print "CLASS0_FOUR_COVER_COUNT", #four;
+print "CLASS0_FOUR_DESCENT_FINISHED";
